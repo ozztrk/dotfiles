b194d085dc991c88cf39068d872b5e6ccc33a3cf
Ozan Öztürk <ozan.ztuerk@gmail.com>
2024-04-24 11:34:19 +0200

    update

--- nvim/lua/pluginlist.lua
+++ nvim/lua/pluginlist.lua
@@ -1,12 +1,46 @@
 --nvim/lua/pluginlist.lua
 
 return {
-	
+
 	{
 		"numToStr/Comment.nvim",
 		config = function()
 			require("Comment").setup()
 		end
 	},
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme catppuccin")
+		end
+	},
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function()
+			require("lualine").setup({ icons_enables = true,
+				theme = 'catppuccin',
+			})
+		end,
+	},
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	'folke/neodev.nvim', -- new
+
+	-- for what is that for?
+	{
+		'hrsh7th/nvim-cmp',
+		dependencies = {
+			'L3MON4D3/LuaSnip',
+			'saadparwaiz1/cmp_luasnip',
+			'rafamadriz/friendly-snippets',
+
+			'hrsh7th/cmp-nvim-lsp',
+
+		},
+	},
+
 }
